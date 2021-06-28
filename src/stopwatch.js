9a178a6bc54e1146be33e6af3ff839fc21e9cb73
Pavel <player49@mail.ru>
2021-06-28 15:35:24 +0200

    time

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -19,7 +19,7 @@ function Stopwatch() {
     const start = () => { // const
         run();
         setStatus(1);
-        setInterv(setInterval(run, 600));
+        setInterv(setInterval(run, 300));
     };
 
     var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
